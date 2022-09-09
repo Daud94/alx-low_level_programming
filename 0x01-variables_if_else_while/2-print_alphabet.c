5012da4e54d5329313a9ff858e4f6c6e7cdc2cb3
Daud Olarewaju <olarewajuyahyadaud@gmail.com>
2022-09-09 22:32:25 +0100

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+/* Program to determing if a random number is positive, negative or zero */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -Main function entry point
+ * Return: Always zero
+ */
+int main(void) /* The function returns void */
+{
+	char letter = 'a';
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	return (0);
+}
