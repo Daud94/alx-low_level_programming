1cdc8aee8aa9f49ca871375e0e363f1c80bb9328
Daud Olarewaju <olarewajuyahyadaud@gmail.com>
2022-09-09 23:12:20 +0100

    base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+/* Program to determing if a random number is positive, negative or zero */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -Main function entry point
+ * Return: Always zero
+ */
+int main(void) /* The function returns void */
+{
+	int num;
+
+	for (num = 0; num < 10; num)
+		putchar(num);
+	putchar('\n');
+	return (0);
+}
