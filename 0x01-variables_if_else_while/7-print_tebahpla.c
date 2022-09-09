37b2191a2ea71a821a1a0fd6c73dd664c1169c7f
Daud Olarewaju <olarewajuyahyadaud@gmail.com>
2022-09-09 23:49:40 +0100

    alphabet reversed

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+/* Program to determing if a random number is positive, negative or zero */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -Main function entry point
+ * Return: Always zero
+ */
+int main(void) /* The function returns void */
+{
+	char letter = 'z';
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
