4f9fbc7bacf53f2ab5f1be36a5362655274d3d7e
Daud Olarewaju <olarewajuyahyadaud@gmail.com>
2022-09-09 23:07:59 +0100

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+/* Program to determing if a random number is positive, negative or zero */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -Main function entry point
+ * Return: Always zero
+ */
+int main(void) /* The function returns void */
+{
+	char letter = 'a';
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter == 'q' || letter == 'e')
+		{
+			continue;
+		}
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
