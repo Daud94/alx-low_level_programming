7d1e713a4f71da0fb67432e7450d6fce24e30507
Daud Olarewaju <olarewajuyahyadaud@gmail.com>
2022-09-13 11:59:26 +0100

    Sign

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -53,4 +53,24 @@ int _isalpha(int c)
 		return 0;
 	}
 }
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return 1;
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return 0;
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return -1;
+	}
+	return (0);
+}
+
 #endif
