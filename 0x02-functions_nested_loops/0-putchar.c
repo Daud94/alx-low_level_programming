b2663665256bdabc36bce65421d78a5a0b636561
Daud Olarewaju <olarewajuyahyadaud@gmail.com>
2022-09-13 09:47:01 +0100

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#inlude "main.h"
+
+/**
+ * main - Main function entry point
+ * Return: Always zero
+ */
+int main(void) /* The function returns void */
+{
+	char str_arr[8] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(str_arr[i]);
+	}
+
+	return (0);
+}
