e33575bb4208bd940d098ef98e8d76e922a34235
Daud Olarewaju <olarewajuyahyadaud@gmail.com>
2022-09-13 16:15:31 +0100

    lowercase alphabet

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - Main function entry point
+ * Return: Always zero
+ */
+int _islower(int c) /* The function returns void */
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
