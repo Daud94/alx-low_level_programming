ad3694aa9d943e271832a8fafcc9df0caabe5c0b
Daud Olarewaju <olarewajuyahyadaud@gmail.com>
2022-09-13 16:21:50 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower - Main function entry point
+ * _islower(int c) - Main function entry point
  * Return: Always zero
  */
 int _islower(int c) /* The function returns void */
